089778a7abece39185e928e3ed0dd7e77e33a7b6
Damirhub <damir.pjetlovic@gmail.com>
2019-07-25 14:29:29 +0200

    init components, added router

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
+import WbnPlayer from './WbnPlayer';
 
 const App = () => (
-  <div>Start here!</div>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path ="/" component = {WbnPlayer}/>
+      <Route exact path = "/activeVider" component = {WbnPlayer}/>
+    </Switch>
+  </BrowserRouter>
 )
 
 export default App;
